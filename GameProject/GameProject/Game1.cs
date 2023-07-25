1114335f9e54b54cf3cc6da6da802d6ef16b1a2d
RafVanhoegaerden-s128965 <s128965@ap.be>
2023-07-25 14:44:19 +0200

    Refactor in Game class the screen width/height

--- GameProject/GameProject/Game1.cs
+++ GameProject/GameProject/Game1.cs
@@ -14,40 +14,32 @@ namespace GameProject
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        private static Screen _screen;
-        private static int _screenWidth;
-        private static int _screenHeight;
-
-        private Texture2D _blockTexture;
-
         //Main Character
         private MainCharacter _mainCharacter;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+
+            #region Screen
             _graphics.IsFullScreen = false;
 
             // Screen WIDTH
-            _graphics.PreferredBackBufferWidth = 1280;
-            _screenWidth = _graphics.PreferredBackBufferWidth;
+            _graphics.PreferredBackBufferWidth = Settings.Screen.Width;
 
             // Screen HEIGHT
-            _graphics.PreferredBackBufferHeight = 720;
-            _screenHeight = _graphics.PreferredBackBufferHeight;
+            _graphics.PreferredBackBufferHeight = Settings.Screen.Height;
 
             _graphics.ApplyChanges();
+            #endregion
 
             Content.RootDirectory = "Content";
 
             IsMouseVisible = true;
-
         }
 
         protected override void Initialize()
         {
-            _screen = new Screen(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
-
             // TODO: Add your initialization logic here
 
             // Add objects to MovementManagerList
